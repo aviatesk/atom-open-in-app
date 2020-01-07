db6ae290447507f16bdcbd505e95c76d7e5e185b
aviatesk <aviatesk@gmail.com>
2020-01-08 02:18:39 +0900

    fix JS style

--- open-in-app.js
+++ open-in-app.js
@@ -1,50 +1,52 @@
 /** @babel */
 
-import * as Atom from "atom";
+import { CompositeDisposable } from "atom";
 import open from "open";
 import path from "path";
 
 class OpenInApp {
-  constructor() {
-    this.subscriptions = new Atom.CompositeDisposable();
-  }
   activate() {
-    // Subscriptions
-    this.subscriptions.add(atom.commands.add("atom-text-editor", "open-in-app:open-file-from-editor", this.openFileFromEditor.bind(this)));
-    this.subscriptions.add(atom.commands.add(".tree-view", "open-in-app:open-file-from-tree", this.openFileFromTree.bind(this)));
-    this.subscriptions.add(atom.commands.add(".tree-view .file", "open-in-app:open-file-on-tree-click", this.openFileFromTree.bind(this)));
+    this.subscriptions = new CompositeDisposable();
+    this.subscriptions.add(
+      atom.commands.add("atom-text-editor", "open-in-app:open-file-from-editor", () => {
+        this.openFileFromEditor();
+      }),
+      atom.commands.add(".tree-view", "open-in-app:open-file-from-tree", () => {
+        this.openFileFromTree();
+      }),
+      atom.commands.add(".tree-view .file", "open-in-app:open-file-on-tree-click", () => {
+        this.openFileFromTree();
+      })
+    );
   }
+
   deactivate() {
     this.subscriptions.dispose();
   }
+
   openFileFromEditor() {
     const filePath = this.getFilePath();
-    if (!filePath) {
-      return;
-    }
+    if (!filePath) return;
     this.openFile(filePath);
   }
+
   openFileFromTree() {
     const treeView = atom.packages.getActivePackage("tree-view");
-    if (!treeView) {
-      return;
-    }
+    if (!treeView) return;
     const filePath = treeView.mainModule.treeView.selectedPath;
-    if (!filePath) {
-      return;
-    }
+    if (!filePath) return;
     this.openFile(filePath);
   }
+
   getFilePath() {
     const editor = atom.workspace.getActiveTextEditor();
-    if (!editor) {
-      return;
-    } else {
-      return editor.getPath();
-    }
+    if (!editor) return null;
+    return editor.getPath();
   }
+
   openFile(filePath) {
     const ext = path.extname(filePath);
+
     let applicationBindings;
     try {
       applicationBindings = JSON.parse(atom.config.get("open-in-app.applicationBindings"));
@@ -61,12 +63,11 @@ class OpenInApp {
         return;
       }
     }
-    let openApp;
-    if (applicationBindings.hasOwnProperty(ext)) {
-      openApp = applicationBindings[ext];
-    } else {
-      openApp = atom.config.get("open-in-app.defaultApplication");
-    }
+
+    const openApp = applicationBindings.hasOwnProperty(ext) ?
+      applicationBindings[ext] :
+      atom.config.get("open-in-app.defaultApplication");
+
     open(filePath, { app: openApp });
   }
 }
