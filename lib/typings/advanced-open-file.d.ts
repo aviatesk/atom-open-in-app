d4d0857665ecc5399023d103fdf3638a5a6aae74
aviatesk <aviatesk@gmail.com>
2020-01-08 01:42:27 +0900

    No TS where I don't really need

--- lib/typings/advanced-open-file.d.ts
+++
@@ -1,8 +0,0 @@
-import * as Atom from "atom";
-
-declare type Callback = (filePath: string) => void;
-
-declare type AdvancedOpenFileEventService = {
-  onDidOpenPath: (callback: Callback) => Atom.Disposable;
-  onDidCreatePath: (callback: Callback) => Atom.Disposable;
-}
